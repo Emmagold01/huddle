d5c18a0baabcfc108944cbe5bae3e79bc7228de6
soyooye Emmanuel <soyooyeemmanuel@gmail.com>
2025-01-02 01:04:16 +0100

    changed p color

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 const darkModeHeader = document.querySelector('body');
 const darkModeButton = document.querySelector('.dark');
 const logoImage = document.querySelector('.logo img');
+const paragraph = document.querySelectorAll('p');
 
 function toggleDarkMode() {
   darkModeHeader.classList.toggle('body2');
 
   if (darkModeHeader.classList.contains('body2')) {
     logoImage.src = 'images/logo2.svg';
+    paragraph.forEach((p) => (p.style.color = '#9e9fa1'));
   } else {
     logoImage.src = 'images/logo.svg';
+    paragraph.forEach((p) => (p.style.color = '##3e414c'));
   }
 }
 
